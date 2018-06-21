f3797093392a869991b55a420a2b3a3b38ee57e6
Matt Greff <greff.matt@icloud.com>
2018-06-21 02:29:20 +0000

    add user login auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  
   private
 
   def cart
@@ -18,5 +23,6 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
 
 end
